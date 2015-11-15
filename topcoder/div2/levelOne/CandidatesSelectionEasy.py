57aca0346b7c58b5e6548f295658429cd6e19d98
orestisfoufris <orestis.foufris@betfair.com>
2015-11-15 21:26:59 +0000

    removed assert

--- topcoder/div2/levelOne/CandidatesSelectionEasy.py
+++ topcoder/div2/levelOne/CandidatesSelectionEasy.py
@@ -14,9 +14,3 @@ def sort(scores, x):
 
     tuples = sorted(tuples, key=operator.itemgetter(0))
     return [tuples[x][1] for x in range(len(tuples))]
-
-assert(sort(["ACB", "BAC", "CBA"], 1) == [1, 2, 0])
-assert(sort(["A", "C", "B", "C", "A"], 0) == [0, 4, 2, 1, 3])
-assert(sort(["LAX","BUR","ONT","LGB","SAN","SNA","SFO","OAK","SJC"], 2) == [5, 3, 8, 7, 4, 6, 1, 2, 0])
-assert(sort(["XXYWZWWYXZ","YZZZYWYZYW","ZYZZWZYYWW","ZWZWZWZXYW","ZYXWZXWYXY","YXXXZWXWXW","XWWYZWXYXY","XYYXYWYXWY","ZZYXZYZXYY","WXZXWYZWYY"], 3) == [0, 3, 4, 5, 7, 8, 9, 6, 1, 2])
-assert(sort(["X"], 0) == [0])
